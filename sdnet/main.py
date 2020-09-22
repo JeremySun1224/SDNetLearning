fe8538de85d1dddc5c077b10c7d696bd45e99b39
jeremysun1224 <2113751293@qq.com>
2020-09-22 19:31:14 +0800

    added the main.py

---
+++ sdnet/main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# -*- author: JeremySun -*-
+# -*- dating: 20/9/22 -*-
+
+import argparse
+import os
+import sys
+import torch
+from Models.SDNetTrainer import SDNetTrainer
+from Utils.Arguments import Arguments
+
+opt = None
+    

786834032e035b290252a0765308c50ab6c033b1
jeremysun1224 <2113751293@qq.com>
2020-09-26 10:16:36 +0800

    added the main.py

--- sdnet/main.py
+++ sdnet/main.py
@@ -13,3 +13,6 @@ opt = None
 
 # argparse可以让我们直接在命令行中就可以向程序中传入参数并让程序运行
 parser = argparse.ArgumentParser(description='SDNet')
+parser.add_argument('command', help='Command: train')
+parser.add_argument('conf_file', help='Path to conf file.')
+

b1f1829b0bd48c066909d9ff23909deda23bd16f
jeremysun1224 <2113751293@qq.com>
2020-10-05 17:38:12 +0800

    added the test file

--- sdnet/Test/test.py
+++ sdnet/Test/test.py
@@ -3,20 +3,21 @@
 # -*- dating: 20/9/16 -*-
 
 import torch
+import sys
 use_cuda = ['GPU:0' if torch.cuda.is_available() else 'CPU']
 
-def gen_upper_triangle(score_s, score_e, max_len, use_cuda):
-    batch_size = score_s.shape[0]
-    context_len = score_s.shape[1]  # context_word_num
-    expand_score = score_s.unsqueeze(2).expand([batch_size, context_len, context_len]) + \
-                   score_e.unsqueeze(1).expand([batch_size, context_len, context_len])
-    score_mask = torch.ones(context_len)
-    if use_cuda:
-        score_mask = score_mask.cuda()
-    score_mask = torch.ger(score_mask, score_mask).triu().tril(max_len - 1)
-    empty_mask = score_mask.eq(0).unsqueeze(0).expand_as(expand_score)
-    expand_score.data.masked_fill_(empty_mask.data, -float('inf'))
-    return expand_score.contiguous().view(batch_size, -1)  # batch x context_len x context_len
+# def gen_upper_triangle(score_s, score_e, max_len, use_cuda):
+#     batch_size = score_s.shape[0]
+#     context_len = score_s.shape[1]  # context_word_num
+#     expand_score = score_s.unsqueeze(2).expand([batch_size, context_len, context_len]) + score_e.unsqueeze(1).expand([batch_size, context_len, context_len])
+#     score_mask = torch.ones(context_len)
+#     if use_cuda:
+#         score_mask = score_mask.cuda()
+#     score_mask = torch.ger(score_mask, score_mask).triu().tril(max_len - 1)
+#     empty_mask = score_mask.eq(0).unsqueeze(0).expand_as(expand_score)
+#     expand_score.data.masked_fill_(empty_mask.data, -float('inf'))
+#     return expand_score.contiguous().view(batch_size, -1)  # batch x context_len x context_len
 
-
-
+print(torch.__version__)
+print(sys.executable)
+print(torch.ones(2, 3))
